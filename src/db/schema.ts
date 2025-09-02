e5548a94a19723810f6d6865339e9a0084ca71bc
JorgeWendell <jorge.wendell@adelbr.tech>
2025-09-02 15:38:13 -0300

    chore: better-auth setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -32,8 +32,48 @@ export const UsersTable = pgTable("users", {
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const sessionsTable = pgTable("sessions", {
+  id: text("id").primaryKey(),
+  expiresAt: timestamp("expires_at").notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
+    .notNull()
+    .references(() => UsersTable.id, { onDelete: "cascade" }),
+});
+
+export const accountsTable = pgTable("accounts", {
+  id: text("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => UsersTable.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const verificationsTable = pgTable("verifications", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at"),
+  updatedAt: timestamp("updated_at"),
+});
+
 // Tabela de stock
-export const StockTable = pgTable("stock", {
+export const StocksTable = pgTable("stocks", {
   id: text("id").primaryKey(),
   userId: text("user_id")
     .notNull()
@@ -42,6 +82,7 @@ export const StockTable = pgTable("stock", {
   dateEntry: timestamp("date_entry").notNull().defaultNow(),
   dateExit: timestamp("date_exit"),
   priceInCents: integer("price_in_cents").notNull(),
+  quantity: integer("quantity").notNull().default(1),
   depto: departmentEnum("depto").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
@@ -50,5 +91,5 @@ export const StockTable = pgTable("stock", {
 // Tipos para TypeScript
 export type User = typeof UsersTable.$inferSelect;
 export type NewUser = typeof UsersTable.$inferInsert;
-export type Stock = typeof StockTable.$inferSelect;
-export type NewStock = typeof StockTable.$inferInsert;
+export type Stock = typeof StocksTable.$inferSelect;
+export type NewStock = typeof StocksTable.$inferInsert;
