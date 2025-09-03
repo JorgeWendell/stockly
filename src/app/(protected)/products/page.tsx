770622534c41b66a4114ee4104223724d1b9471f
JorgeWendell <jorge.wendell@adelbr.tech>
2025-09-03 16:05:02 -0300

    feat: create page container components

--- src/app/(protected)/products/page.tsx
+++ src/app/(protected)/products/page.tsx
@@ -1,5 +1,35 @@
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { Plus } from "lucide-react";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Estoque de Produtos</PageTitle>
+          <PageDescription>Gerencie seus produtos</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar Produto
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Produtos</h1>
+      </PageContent>
+    </PageContainer>
+  );
 };
 
 export default ProductsPage;
